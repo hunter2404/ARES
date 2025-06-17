2007cd7e900fcf8398f01e35efc1daa4d5487177
hunter2404 <145696237+hunter2404@users.noreply.github.com>
2025-06-17 14:44:20 +0000

    Point settings module at anubis.anubis.settings

--- anubis/anubis/wsgi.py
+++ anubis/anubis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anubis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anubis.anubis.settings')
 
 application = get_wsgi_application()
