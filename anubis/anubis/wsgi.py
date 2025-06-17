12690e5e40c5b3bec3cd9170b7798fa083c08232
hunter2404 <145696237+hunter2404@users.noreply.github.com>
2025-06-17 14:30:56 +0000

    Revert DJANGO_SETTINGS_MODULE back to anubis.settings

--- anubis/anubis/wsgi.py
+++ anubis/anubis/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anubis.anubis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'anubis.settings')
 
 application = get_wsgi_application()
